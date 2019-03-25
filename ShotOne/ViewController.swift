f78088fa10d4b258e57b272c648515b614601a42
Ivan Zinovyev <zinovyev-ivan@yandex.ru>
2019-03-25 20:05:44 +0300

    Initial Commit

---
+++ ShotOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShotOne
+//
+//  Created by Ivan Zinovyev on 25/03/2019.
+//  Copyright © 2019 Ivan Zinovyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
